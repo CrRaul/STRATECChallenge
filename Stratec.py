39e0fbf7ebaea2c06f0f0cc851a8317b756c3757
CrRaulgit config --global user.name CrRaul <cr.iraul@yahoo.comgit config --global user.email cr.iraul@yahoo.com>
2019-03-23 11:50:02 +0200

    123

--- Stratec.py
+++ Stratec.py
@@ -1,14 +1,13 @@
 import numpy as np
-
-
-
-
- 
+from queue import Queue
+import random
 
 class Stratec():
 	def __init__(self):
 		self.matrix = None
-			
+		self.solLvl1 = None	
+		self.solLvl2 = None		
+		self.path = None
 
 	def loadCSV(self, path):
 		self.matrix = np.genfromtxt(path,delimiter=',')
@@ -16,29 +15,314 @@ class Stratec():
 	def getMatrix(self):
 		return self.matrix
 
+
        ##################################################################	Level 1
        #         							#   
-	
-	def lee(self):
-		pass
+	def checkMarkNoise(self, start):
+		q = Queue()
+		q.put(start)
 		
+		stepDirections = [0,0,0,0]		
+		direction = []
+
+		self.matrix[start[0],start[1]] = 2
+
+		while not q.empty():
+			index = q.get()	
+			
+			l = (index[0], index[1]-1)
+			r = (index[0], index[1]+1)
+			u = (index[0]-1, index[1])
+			d = (index[0]+1, index[1])
+
+
+			if l[1] >= 0 and self.matrix[l[0]][l[1]] == 1:
+
+				stepDirections[0] += 1 
+				direction.append(0)
+				q.put(l)
+				self.matrix[l[0],l[1]] = 2				
+						
+
+			if r[1] < len(self.matrix[0]) and self.matrix[r[0]][r[1]] == 1:
+
+				stepDirections[1] += 1
+				direction.append(1)
+				q.put(r)
+				self.matrix[r[0],r[1]] = 2
+
+
+			if u[0] >= 0 and self.matrix[u[0]][u[1]] == 1:
+
+				stepDirections[2] += 1
+				direction.append(2)
+				q.put(u)
+				self.matrix[u[0],u[1]] = 2
+
+			if d[0] < len(self.matrix) and self.matrix[d[0]][d[1]] == 1:
+
+				stepDirections[3] += 1
+				direction.append(3)
+				q.put(d)
+				self.matrix[d[0],d[1]] = 2
+
+
+		num = 0
+		for i in range(4):
+			if stepDirections[i] >= 1:
+				num += 1
+
+		if num == 1 or num == 0:
+			self.matrix[start[0],start[1]] = -1
+			for i in range(len(direction)):
+				if direction[i] == 0:
+					start = (start[0], start[1] - 1) 
+
+				if direction[i] == 1: 
+					start = (start[0], start[1] + 1) 
+
+				if direction[i] == 2: 
+					start = (start[0] - 1, start[1]) 
+
+				if direction[i] == 3: 
+					start = (start[0] + 1, start[1]) 
+				self.matrix[start[0],start[1]] = -1
+			return True
+		return False
+				
 	
 	def levelOne(self):
-		self.loadCSV('The_Basics.csv')	
-		
+		if self.path != 3:
+			self.loadCSV('The_Basics.csv')	
+		self.path = None		
+
 		shape = self.matrix.shape
 		wM = shape[1]
 		hM = shape[0]		
-
 		
-				
+		numNoise = numTotalS = 0
 		
+		for i in range(hM):
+			for j in range(wM):
+				if self.matrix[i][j] == 1:
+					if self.checkMarkNoise((i,j)) == True:	
+						numNoise += 1
+					numTotalS +=1
+		#self.checkMarkNoise((9,18))	
 
-	
+		# write the solution into file			
+		text_file = open("lvl1.txt", "w")
+		text_file.write(str(numTotalS-numNoise))
+		text_file.close()
+		self.solLvl1 = numTotalS-numNoise
 
+		return numTotalS-numNoise
        #								 #
        ###################################################################
 
 
-str = Stratec()
-str.levelOne()
+
+	##################################################################	Level 2
+       #         							#   
+	def checkMarkBox(self, start):
+		q = Queue()
+		q.put(start)
+		
+		xMin, yMin = 100,100
+		xMax, yMax = -1,-1
+
+		self.matrix[start[0],start[1]] = 3
+
+		while not q.empty():
+			index = q.get()	
+			
+			l = (index[0], index[1]-1)
+			r = (index[0], index[1]+1)
+			u = (index[0]-1, index[1])
+			d = (index[0]+1, index[1])
+
+			if index[1] < xMin:
+				xMin = index[1]			
+			if index[1] > xMax:
+				xMax = index[1]
+			if index[0] < yMin:
+				yMin = index[0]				
+			if index[0] > yMax:
+				yMax = index[0]
+
+			if l[1] >= 0 and self.matrix[l[0]][l[1]] == 2:
+				q.put(l)
+				self.matrix[l[0],l[1]] = 3				
+						
+
+			if r[1] < len(self.matrix[0]) and self.matrix[r[0]][r[1]] == 2:
+				q.put(r)
+				self.matrix[r[0],r[1]] = 3
+
+
+			if u[0] >= 0 and self.matrix[u[0]][u[1]] == 2:
+				q.put(u)
+				self.matrix[u[0],u[1]] = 3
+
+
+			if d[0] < len(self.matrix) and self.matrix[d[0]][d[1]] == 2:
+				q.put(d)
+				self.matrix[d[0],d[1]] = 3
+	
+	
+		if xMin == xMax and yMin == yMax: 
+			return [0,0,0,0]
+
+		return [xMin,yMin,xMax,yMax]
+	
+	def levelTwo(self):
+		if self.path != 3:
+			self.loadCSV('The_Basics.csv')
+
+		self.solLvl1 = self.levelOne()
+		self.path = None
+
+		shape = self.matrix.shape
+		wM = shape[1]
+		hM = shape[0]		
+		
+		sol = []
+		
+		for i in range(hM):
+			for j in range(wM):
+				if self.matrix[i][j] == 2:
+					s = self.checkMarkBox((i,j))
+
+					if s[0] == 0 and s[1] == 0 and s[2] == 0 and s[3] == 0:
+						continue
+					else:
+						sol.append(s)	
+		
+		# write the solution into file		
+		text_file = open("lvl2.txt", "w")
+		text_file.write(str(self.solLvl1)+"\n")
+
+		for i in range(len(sol)):
+			xL = sol[i][0]
+			yL = sol[i][1]        
+			xR = sol[i][2]
+			yR = sol[i][3]
+			
+			text_file.write("("+str(yL)+","+str(xL)+") W:"+str(xR-xL+1)+" H:"+str(yR-yL+1)+"\n")
+
+
+		text_file.close()
+		return sol
+       #								 #
+       ###################################################################
+
+
+       ##################################################################	Level 3
+       #         							#   
+	def checkEquMatrix(self, posM1, posM2):
+		A = self.matrix[posM1[1]:posM1[3]+1,posM1[0]:posM1[2]+1]
+		B = self.matrix[posM2[1]:posM2[3]+1,posM2[0]:posM2[2]+1]
+		
+		shapeA = A.shape
+		shapeB = B.shape
+
+		if shapeA[0] != shapeB[0] or shapeA[1] != shapeB[1]:
+			return False
+	
+		for i in range(0,shapeA[0]):
+			for j in range(0,shapeA[1]):
+				if A[i][j] != B[i][j]:
+					return False
+
+		return True
+
+	def fill(self, start, colorList, color = None):
+		q = Queue()
+		q.put(start)
+
+		if color == None:
+			color = random.randint(100,255)
+			while color in colorList:
+				color = random.randint(0,255)
+
+		self.matrix[start[0],start[1]] = color
+
+		while not q.empty():
+			index = q.get()	
+			
+			l = (index[0], index[1]-1)
+			r = (index[0], index[1]+1)
+			u = (index[0]-1, index[1])
+			d = (index[0]+1, index[1])
+
+
+			if l[1] >= 0 and self.matrix[l[0]][l[1]] == 3:
+				q.put(l)
+				self.matrix[l[0],l[1]] = color				
+						
+
+			if r[1] < len(self.matrix[0]) and self.matrix[r[0]][r[1]] == 3:
+				q.put(r)
+				self.matrix[r[0],r[1]] = color
+
+
+			if u[0] >= 0 and self.matrix[u[0]][u[1]] == 3:
+				q.put(u)
+				self.matrix[u[0],u[1]] = color
+
+
+			if d[0] < len(self.matrix) and self.matrix[d[0]][d[1]] == 3:
+				q.put(d)
+				self.matrix[d[0],d[1]] = color
+		return color
+		
+
+	def levelThree(self):
+
+		self.loadCSV('Duplicates.csv')
+		self.path = 3
+		self.solLvl2 = self.levelTwo()
+	
+		sol = self.solLvl2	
+		
+		# write the solution into file		
+		text_file = open("lvl3.txt", "w")
+		text_file.write(str(self.solLvl1)+"\n")
+		
+		colorList = []		
+		
+
+		for i in range(len(sol)-1):
+			dupList = []
+			for j in range(i+1,len(sol)):
+				if self.checkEquMatrix(sol[i],sol[j]):
+					dupList.append(j)
+			
+			xL = sol[i][0]
+			yL = sol[i][1]        
+			xR = sol[i][2]
+			yR = sol[i][3]
+
+			if len(dupList) == 0:	
+				text_file.write("("+str(yL)+","+str(xL)+") W:"+str(xR-xL+1)+" H:"+str(yR-yL+1)+"\n")
+			else:
+				color = self.fill((sol[i][1],sol[i][0]),colorList)
+				colorList.append(color)
+				
+				text_file.write("("+str(yL)+","+str(xL)+") W:"+str(xR-xL+1)+" H:"+str(yR-yL+1))
+				for k in range(len(dupList)):
+					xL2 = sol[dupList[k]][0]
+					yL2 = sol[dupList[k]][1]
+
+					color = self.fill((sol[dupList[k]][1],sol[dupList[k]][0]),colorList, color)				
+
+					text_file.write(" this object is also found at ""("+str(yL2)+","+str(xL2)+")"+ "\n")
+					
+					sol[dupList[k]][0] = 0
+					sol[dupList[k]][1] = 0
+					sol[dupList[k]][2] = 0
+					sol[dupList[k]][3] = 0
+		text_file.close()
+       #								 #
+       ###################################################################
+
--- gui.py
+++ gui.py
@@ -1,10 +1,10 @@
 from Stratec import *
 
 from tkinter import *
+import tkinter as tki
 from PIL import ImageTk, Image
-from tkinter import filedialog
-import ntpath
 import cv2
+
 import numpy as np
 
 class Window(Frame):
@@ -22,30 +22,155 @@ class Window(Frame):
 
         self.iW = 950
         self.iH = 450
+	
+	# draw the number range ( up - down). Static
+        for i in range(self.iH//10):
+            w = tki.Label(self.master, text = i)
+            w.config(font=("Courier", 6))
+            w.pack()
+            w.place(x = 10, y = i*10+25)
+
+            w = tki.Label(self.master, text = i)
+            w.config(font=("Courier", 6))
+            w.pack()
+            w.place(x = 980, y = i*10+25)
+	
+	# draw the number range ( left - right). Static
+        for i in range(self.iW//10):
+            w = tki.Label(self.master, text = i)
+            w.config(font=("Courier", 5))
+            w.pack()
+            w.place(x = i*10+25, y = 10)
+		
+            w = tki.Label(self.master, text = i)
+            w.config(font=("Courier", 5))
+            w.pack()
+            w.place(x = i*10+25, y = 480)
 
         self.init_window()
     #############################################################		LVL 1
 
-    def levelOne(self):
+    def drawLevelOne(self):
         im = np.zeros((self.iH, self.iW, 3), np.uint8)
 	
-        self.stratec.levelOne()
+	# solve lvl 1 and save the result
+        sol = self.stratec.levelOne()
 	
+	# get the modified matrix
         matrix = self.stratec.getMatrix()
 	
-
+	# draw the matrix
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
-                if matrix[i][j] == 1:
-                    im[i*10:i*10+10,j*10:j*10+10] = 255
-               
-                cv2.line(im, (j*10, 0), (j*10, self.iH), (80,80,80), 1)
-            cv2.line(im, (0, i*10), (self.iW, i*10), (80,80,80), 1)
+
+                if matrix[i][j] == 2:
+                    im[i*10:i*10+10, j*10:j*10+10, 1] = 255           
+		
+                if matrix[i][j] == -1:
+                    im[i*10:i*10+10, j*10:j*10+10, 2] = 255      
+
+                cv2.line(im, (j*10, 0), (j*10, self.iH), (50,50,50), 1)
+            cv2.line(im, (0, i*10), (self.iW, i*10), (50,50,50), 1)
         
+        
+        w = tki.Label(self.master, text = "                Number of non-noise objects: ")
+        w.pack()
+        w.place(x = 310, y = 495)
+	
+        w = tki.Label(self.master, text = str(sol)+"                ")
+        w.pack()
+        w.place(x = 560, y = 495)
+	
+	
         self.image = im
         self.updatePanel()
 
+    #############################################################    
+
+    #############################################################		LVL 2
+
+    def drawLevelTwo(self):
+        im = np.zeros((self.iH, self.iW, 3), np.uint8)
 	
+	# solve lvl 2 and save the result
+        sol = self.stratec.levelTwo()
+	
+	# get the modified matrix
+        matrix = self.stratec.getMatrix()
+	
+	# draw the matrix
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+
+                if matrix[i][j] == 3:
+                    im[i*10:i*10+10, j*10:j*10+10, 1] = 255           
+		
+                if matrix[i][j] == -1:
+                    im[i*10:i*10+10, j*10:j*10+10, 2] = 255      
+
+                cv2.line(im, (j*10, 0), (j*10, self.iH), (50,50,50), 1)
+            cv2.line(im, (0, i*10), (self.iW, i*10), (50,50,50), 1)
+        
+	
+        for i in range(len(sol)):
+            xL = sol[i][0]*10
+            yL = sol[i][1]*10        
+            xR = sol[i][2]*10+10
+            yR = sol[i][3]*10+10
+
+            cv2.line(im, (xL,yL), (xL,yR), (255,0,0),2)
+            cv2.line(im, (xL,yL), (xR,yL), (255,0,0),2)
+            cv2.line(im, (xL,yR), (xR,yR), (255,0,0),2)
+            cv2.line(im, (xR,yL), (xR,yR), (255,0,0),2)
+        
+        w = tki.Label(self.master, text = "                Solution is in \"lvl2.txt\"                ")
+        w.pack()
+        w.place(x = 360, y = 495)
+	
+	
+        self.image = im
+        self.updatePanel()
+
+    #############################################################    
+
+
+    #############################################################		LVL 3
+
+    def drawLevelThree(self):
+        im = np.zeros((self.iH, self.iW, 3), np.uint8)
+	
+	# solve lvl 3
+        self.stratec.levelThree()
+	
+	# get the modified matrix
+        matrix = self.stratec.getMatrix()
+	
+	# draw the matrix
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+
+                if matrix[i][j] == 3:
+                    im[i*10:i*10+10, j*10:j*10+10, 1] = 255           
+		
+                elif matrix[i][j] == -1:
+                    im[i*10:i*10+10, j*10:j*10+10, 2] = 255      
+                else:
+                    im[i*10:i*10+10, j*10:j*10+10, 0] = matrix[i][j]
+                    im[i*10:i*10+10, j*10:j*10+10, 2] = matrix[i][j] / ( i + j+1) 
+
+                cv2.line(im, (j*10, 0), (j*10, self.iH), (50,50,50), 1)
+            cv2.line(im, (0, i*10), (self.iW, i*10), (50,50,50), 1)
+       
+        im[0:10,0:10][0],im[0:10,0:10][1],im[0:10,0:10][2] = 0,0,0 
+
+        w = tki.Label(self.master, text = "                Solution is in \"lvl3.txt\"                ")
+        w.pack()
+        w.place(x = 360, y = 495)
+	
+	
+        self.image = im
+        self.updatePanel()
+
     #############################################################    
 
     # update the panel with self.image
@@ -74,12 +199,12 @@ class Window(Frame):
         self.pack(fill=BOTH, expand=1)
 
         menubar = Menu(self.master)
-        
+
         # create a pulldown menu, and add it to the menu bar
         filemenu = Menu(menubar, tearoff=0)
-        filemenu.add_command(label="lvl1", command=self.levelOne)
-        filemenu.add_command(label="lvl2", command=self.nothing)
-        filemenu.add_command(label="lvl3", command=self.nothing)
+        filemenu.add_command(label="lvl1", command=self.drawLevelOne)
+        filemenu.add_command(label="lvl2", command=self.drawLevelTwo)
+        filemenu.add_command(label="lvl3", command=self.drawLevelThree)
         filemenu.add_command(label="lvl4", command=self.nothing)
         filemenu.add_separator()
         filemenu.add_command(label="Exit", command= self.master.quit)
@@ -90,9 +215,9 @@ class Window(Frame):
 ######################################################     ^
     
 
-root = Tk()
+root = tki.Tk()
 
-root.geometry("1000x500")
+root.geometry("1000x520")
 
 #creation of an instance
 app = Window(root)
